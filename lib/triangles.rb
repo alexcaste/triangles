a9f0b9765add956a6694ff7f76d39d478be1db47
alex caste & Cody Bouscaren <student@epicodus.com>
2015-04-27 14:07:30 -0700

    tri_sca method checks if no sides are equal

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -13,4 +13,8 @@ class Triangle
     define_method(:tri_iso) do
         @side1 == @side2 || @side1 == @side3 || @side2 == @side3
     end
+
+    define_method(:tri_sca) do
+        @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+    end
 end
